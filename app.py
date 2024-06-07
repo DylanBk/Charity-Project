c04d5e10feeba582f928333594c84ecf2c55ca2d
Dylan <dylan.bullock.965@accesscreative.ac.uk>
2024-06-07 17:28:08 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+# --- ERROR HANDLING ---
+
+@app.errorhandler(400) # bad request
+def err400(error):
+    return render_template('error.html', error_type="Bad Request", error_title="Sorry! We cannot process your request.", error_subtitle="Double check your inputs and try again.")
+
+@app.errorhandler(401) # unauthorised
+def err401(error):
+    return render_template('error.html', error_type="Unauthorised Access", error_title="You do not have authorisation to view this content.", error_subtitle="Please log in to access this page.")
+
+@app.errorhandler(403) # forbidden
+def err403(error):
+    return render_template('error.html', error_type="Forbidden", error_title="You do not have access to view this content.", error_subtitle="Please contact us if you believe this to be a mistake.")
+
+@app.errorhandler(404) # not found
+def err404(error):
+    return render_template('error.html', error_type="Resource Not Found", error_title="Sorry! We could not find that page.", error_subtitle="Check the URL or return to the <a href='" + url_for('home') + "'>home page</a>.")
+
+@app.errorhandler(500) # internal server error
+def err500(error):
+    return render_template('error.html', error_type="Internal Server Error", error_title="Sorry, something went wrong on our end.", error_subtitle="Check back later or report the issue at <a href='" + url_for('home') + "'>email or something</a>.")
+
+
+# --- PAGE ROUTING ---
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about-charity')
+def about_charity():
+    return render_template('about-charity.html')
+
+if __name__ == "__main__":
+    app.run()
